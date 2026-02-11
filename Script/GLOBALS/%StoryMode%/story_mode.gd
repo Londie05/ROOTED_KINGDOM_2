3dfecf65364d208d16ad1542ff40dd3d9e461fb1
Postiko <andypogi345@gmail.com>
2026-02-12 03:37:04 +0800

    Restore story_mode.gd changes

--- Script/GLOBALS/%StoryMode%/story_mode.gd
+++ Script/GLOBALS/%StoryMode%/story_mode.gd
@@ -1,21 +1,25 @@
 extends Node
 
 # This program is just a temporary saving file for Story Mode
+var rewards = {
+	"1-2": { "small": 100, "crystal": 2 }
+}
 
-var _CURRENTLY_PLAYING_CHAPTER:= -1
+var multiplier = {
+	"1-2": 2
+}
+
+func stage_label():
+	var value = str(Global._CURRENTLY_PLAYING_CHAPTER) + "-" + str(Global._current_playing_on_stage)
+	return value
+	
+func next_stage():
+	var value = str(Global._CURRENTLY_PLAYING_CHAPTER) + "-" + str(Global._current_playing_on_stage + 1)
+	return value
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass # Replace with function body.
-
-func reset_chapter_I_browse():
-	if _CURRENTLY_PLAYING_CHAPTER > -1:
-		_CURRENTLY_PLAYING_CHAPTER = -1
-		print("Current Chapter: " + str(_CURRENTLY_PLAYING_CHAPTER))
-		
-func declare_chapter(value: int):
-	if _CURRENTLY_PLAYING_CHAPTER == -1:
-		_CURRENTLY_PLAYING_CHAPTER = value
-		print("Current Chapter: " + str(_CURRENTLY_PLAYING_CHAPTER))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # func _process(delta: float) -> void:
